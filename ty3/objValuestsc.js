181a4050f8246df3a5054b62454756e974ef7812
barathviknesh <57832162+barathviknesh@users.noreply.github.com>
2020-01-09 11:29:41 +0530

    Add files via upload

---
+++ ty3/objValuestsc.js
@@ -0,0 +1,4 @@
+var obj = { name: "Rajini", age: 33, hasPets: false };
+for (var key in obj) {
+    console.log(obj[key]);
+}
---
+++ ty3/objValuestsc.ts
@@ -0,0 +1,5 @@
+var obj ={name: "Rajini", age: 33, hasPets : false};
+for(let key in obj)
+{
+	console.log(obj[key]);
+}

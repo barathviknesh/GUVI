62a542cca2630c80ec50f871170e933cea398fbe
barathviknesh <57832162+barathviknesh@users.noreply.github.com>
2020-01-09 11:33:20 +0530

    Add files via upload

---
+++ ts7/animal.ts
@@ -0,0 +1,12 @@
+class animal{
+   let name;
+    type;
+    category;
+
+}
+constructor(){
+    this.name=animalName;
+}
+
+Object animal1 = new animal("dog"); 
+Object animal2 = new animal("cat");    
